424c32146dd0c60cbf2e6f9c4bce0be0109ffe2f
Dennis <dennis.basyrov@gmail.com>
2019-06-12 21:06:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT bears.color FROM bears WHERE [column name] = [value]"
+  "SELECT bears.color COUNT bears.color FROM bears WHERE [column name] = [value]"
   
   
   SELECT bears.color, COUNT(bears.color) FROM bears GROUP BY bears.color ORDER BY COUNT(*) DESC LIMIT 1;"
